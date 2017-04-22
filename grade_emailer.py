6a5be734e0dd28e22924ff9c62d0b1a9f4d092e9
Jackie Mok <jackiedmok@gmail.com>
2017-04-22 14:35:15 -0400

    add quiz scores and slightly adjust formatting

--- grade_emailer.py
+++ grade_emailer.py
@@ -8,21 +8,26 @@ def report_grades( student, gradebk ):
     ######### DESCRIPTION ###########
     Get given student's class data
     and create .txt file with e-mail
-    message content with grades.
+    message content including grades.
     
     ######### ARGUMENTS #############
     student - (String) Name of the
     student, formatted 'Last, First'.
     '''
     
-    # Get student's class data
+    ########### Get class data for student ###########
+    
     student_data = gradebk.loc[gradebk['Name'] == student]
+    
     student_email = student_data.iloc[0][1]
+    
     try:
         student_track = int(student_data['Track'])
     except:
         student_track = 0
-    
+
+
+    ########### Create track message for student ###########
     
     # Get track message for student
     if student_track in [50, 112]:
@@ -34,31 +39,38 @@ def report_grades( student, gradebk ):
         track_msg = 'We do not yet have a confirmed track for you. To confirm your track, please submit a survey response on the course website.'
     
     
-    # Get assignment grades' messages for student
+    ########### Create grades message for student ###########
     
-    # Problem sets' grades
+    # Problem set grades
     hw_msg = range(len(filter(lambda x: 'HW' in x, student_data.columns.values)))
     for i in xrange(len(hw_msg)):
         score = str(student_data['HW' + str(i + 1)].iloc[0])
         score = str.strip(score)
         hw_msg[i] = 'For problem set #' + str(i + 1) + ', you earned a score of: ' + score + '.'
     
-    # Reading responses' grades
-    rr_msg = range(len(filter(lambda y: 'RR' in y, student_data.columns.values)))
+    # Reading response grades
+    rr_msg = range(len(filter(lambda x: 'RR' in x, student_data.columns.values)))
     for i in xrange(len(rr_msg)):
         score = str(student_data['RR' + str(i + 1)].iloc[0])
         score = str.strip(score)
         rr_msg[i] = 'For reading response #' + str(i + 1) + ', you earned a score of: ' + score + '.'
+        
+    # Quiz grades
+    quiz_msg = range(len(filter(lambda x: 'Quiz' in x, student_data.columns.values)))
+    for i in xrange(len(quiz_msg)):
+        score = str(student_data['Quiz' + str(i + 1)].iloc[0])
+        score = str.strip(score)
+        quiz_msg[i] = 'For quiz #' + str(i + 1) + ', you earned a score of: ' + score + '/4.'
 
     
     ########### Create e-mail message for student ###########
     
     # Standard messages
     greeting = 'Hello, History of Mathematics Student!'
-    gen_info = 'The following is a report of the grades currently recorded for your problem sets and reading responses. Please report any errors to Professor ***** or your TA *****. Thank you!'
-    error_msg = 'If you have dropped the course or believe you have received this message in error, please send an e-mail to ***** at *****. Thank you.'
+    gen_info = "Here are your scores so far for problem sets, reading responses, and quizzes/exams. Please let the TA's know if you spot any problems."
+    error_msg = 'If you believe you have received this message in error, please send an e-mail to ***** at *****. Thank you.'
     
-    body = greeting + '\n\n' + gen_info + '\n\n' + str(track_msg) + '\n\n' + "\n".join(str(x) for x in hw_msg) + "\n\n" + "\n".join(str(y) for y in rr_msg) + '\n\n' + error_msg
+    body = greeting + '\n\n\n' + gen_info + '\n\n' + str(track_msg) + '\n\n' + "\n".join(str(x) for x in hw_msg) + "\n\n" + "\n".join(str(x) for x in rr_msg) + '\n\n' + "\n".join(str(x) for x in quiz_msg) + '\n\n' + error_msg
     
     # Export student's message to text file (Filename 'Email-ID.txt')
     file = open('histMath_grade_emails\\' + student_email + '.txt', 'w')
@@ -72,7 +84,7 @@ def generate_reports():
     '''
     ######### DESCRIPTION ###########
     Create .txt files for students'
-    e-mail messages regarding grades.
+    e-mail messages containing grades.
     
     ######### ARGUMENTS #############
     gradebk - (DataFrame) Gradebook.
@@ -86,6 +98,7 @@ def generate_reports():
     # unknown_track = gradebk.loc[~gradebk['Track'].isin([50, 112])]
     
     class_roster = gradebk['Name']
+    
     for i in xrange(len(class_roster)):
         report_grades(class_roster.iloc[i], gradebk)
     return
